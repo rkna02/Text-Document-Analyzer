1f085e303e51e7b99adc108b53fea0159dc9b9a7
JerryShao3234 <72317125+JerryShao3234@users.noreply.github.com>
2021-09-27 20:08:48 -0700

    Task1 and Task 2 Done

--- src/main/java/cpen221/mp1/Document.java
+++ src/main/java/cpen221/mp1/Document.java
@@ -3,12 +3,22 @@ package cpen221.mp1;
 import cpen221.mp1.exceptions.NoSuitableSentenceException;
 import cpen221.mp1.sentiments.SentimentAnalysis;
 
+import javax.print.Doc;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.text.BreakIterator;
+import java.util.*;
+
+import static java.lang.Character.isLetter;
 
 public class Document {
 
     /* ------- Task 0 ------- */
     /*  all the basic things  */
+
+    private StringBuilder DocText = new StringBuilder();
 
     /**
      * Create a new document using a URL
@@ -16,7 +26,19 @@ public class Document {
      * @param docURL the URL with the contents of the document
      */
     public Document(String docId, URL docURL) {
-        // TODO: Implement this constructor
+        try {
+            String documentURL = docURL.toString();
+            Scanner urlScanner = new Scanner(new URL(documentURL).openStream());
+            while (urlScanner.hasNext()) {
+                DocText.append(" ");
+                DocText.append(urlScanner.nextLine());
+                //System.out.println(urlScanner.nextLine());
+            }
+        }
+        catch (IOException ioe) {
+            System.out.println("Problem reading from URL!");
+        }
+        System.out.println("hi");
     }
 
     /**
@@ -26,33 +48,203 @@ public class Document {
      *                 the document
      */
     public Document(String docId, String fileName) {
-        // TODO: Implement this constructor
-    }
+        try {
+            String fileName2 = new String(fileName);
+            // myFile.txt should be in the root directory for your project
 
-    /**
-     * Obtain the identifier for this document
-     * @return the identifier for this document
-     */
-    public String getDocId() {
-        // TODO: Implement this method
-        return null;
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            for (String fileLine = reader.readLine();
+                 fileLine != null;
+                 fileLine = reader.readLine()) {
+                DocText.append(" ");
+                DocText.append(fileLine);
+            }
+            reader.close();
+        }
+        catch (IOException ioe) {
+            System.out.println("Problem reading file!");
+        }
     }
 
     /* ------- Task 1 ------- */
 
     public double averageWordLength() {
-        // TODO: Implement this method
-        return 0.0;
+        String lowDocText = new String(DocText.toString());
+        lowDocText = DocText.toString().toLowerCase();
+        int sum=0;
+        int bad=0;
+        ArrayList<String> wordArrayList = new ArrayList<String>();
+        for(String word : lowDocText.split(" ")){
+            if(word.length() != 0){
+                if(word.length() == 1){
+                    if(((word.charAt(0) >= 'a' && word.charAt(0) <= 'z'))){
+                        wordArrayList.add(word);
+                    }
+                }
+                else {
+                    wordArrayList.add(word);
+                }
+            }
+        }
+
+        for(int i=0; i<wordArrayList.size(); i++){
+            char[] charArray = wordArrayList.get(i).toCharArray();
+            for (int j = 0; j < charArray.length; j++) {
+                char ch = charArray[j];
+                if (!(isLetter(ch))) {
+                    if(!((j==0) || (j==charArray.length-1))){
+                        if((isLetter(charArray[j-1]) && isLetter(charArray[j+1]))){
+                            System.out.println("special");
+
+                        }
+                        else {
+                            bad++;
+                        }
+
+
+                    }
+                    else {
+                        bad++;
+                    }
+
+                }
+
+            }
+            sum += wordArrayList.get(i).length();
+        }
+
+        sum -= bad;
+
+        int wordCount = 0;
+        for(int i=0; i<wordArrayList.size(); i++){
+            wordCount++;
+        }
+        System.out.println(sum);
+        System.out.println(wordCount);
+
+        return (double) sum/(wordCount);
     }
 
     public double uniqueWordRatio() {
-        // TODO: Implement this method
-        return 0.0;
+        String lowDocText = new String(DocText.toString());
+        lowDocText = DocText.toString().toLowerCase();
+        int sum=0;
+        int bad=0;
+        ArrayList<String> wordArrayList = new ArrayList<String>();
+        for(String word : lowDocText.split(" ")){
+            if(word.length() != 0){
+                if(word.length() == 1){
+                    if(((word.charAt(0) >= 'a' && word.charAt(0) <= 'z'))){
+                        wordArrayList.add(word);
+                    }
+                }
+                else {
+                    wordArrayList.add(word);
+                }
+            }
+        }
+        System.out.println("hi");
+
+       /* Map<String, Integer> UNIQ = new HashMap<String, Integer>();
+
+        for (String c : wordArrayList) {
+            if (UNIQ.containsKey(c)) {
+                UNIQ.replace(c, UNIQ.get(c) + 1);
+            } else {
+                UNIQ.put(c, 1);
+            }
+        }
+
+        int uniqueWords = 0;
+        for(String word : UNIQ.keySet()){
+            if(UNIQ.get(word) == 1){
+                uniqueWords++;
+            }
+        }
+
+        return UNIQ.size()/wordArrayList.size();*/
+
+        Set<String> set = new HashSet<String>();
+
+        for (String c : wordArrayList) {
+            StringBuilder line = new StringBuilder();
+            for(int i=0; i<c.length(); i++){
+                if(isLetter(c.charAt(i))){
+                    line.append(c.charAt(i));
+                }
+            }
+            set.add(line.toString());
+        }
+
+        return (double) set.size()/wordArrayList.size();
+
     }
 
     public double hapaxLegomanaRatio() {
-        // TODO: Implement this method
-        return 0.0;
+        String lowDocText = new String(DocText.toString());
+        lowDocText = DocText.toString().toLowerCase();
+        int sum=0;
+        int bad=0;
+        ArrayList<String> wordArrayList = new ArrayList<String>();
+        for(String word : lowDocText.split(" ")){
+            if(word.length() != 0){
+                if(word.length() == 1){
+                    if(((word.charAt(0) >= 'a' && word.charAt(0) <= 'z'))){
+                        wordArrayList.add(word);
+                    }
+                }
+                else {
+                    wordArrayList.add(word);
+                }
+            }
+        }
+        System.out.println("hi");
+
+       /* Map<String, Integer> UNIQ = new HashMap<String, Integer>();
+
+        for (String c : wordArrayList) {
+            if (UNIQ.containsKey(c)) {
+                UNIQ.replace(c, UNIQ.get(c) + 1);
+            } else {
+                UNIQ.put(c, 1);
+            }
+        }
+
+        int uniqueWords = 0;
+        for(String word : UNIQ.keySet()){
+            if(UNIQ.get(word) == 1){
+                uniqueWords++;
+            }
+        }
+
+        return UNIQ.size()/wordArrayList.size();*/
+
+        Map<String, Integer> UNIQ = new HashMap<>();
+
+        for (String c : wordArrayList) {
+            StringBuilder line = new StringBuilder();
+            for(int i=0; i<c.length(); i++){
+                if(isLetter(c.charAt(i))){
+                    line.append(c.charAt(i));
+                }
+            }
+            if(UNIQ.containsKey(line.toString())){
+                UNIQ.put(line.toString(), UNIQ.get(line.toString()) + 1);
+            }
+            else{
+                UNIQ.put(line.toString(), 0);
+            }
+        }
+        int uniqueCount = 0;
+        for(String c : UNIQ.keySet()){
+            if(UNIQ.get(c) == 0){
+                uniqueCount++;
+            }
+        }
+
+
+
+        return (double) uniqueCount/wordArrayList.size();
     }
 
     /* ------- Task 2 ------- */
@@ -62,10 +254,22 @@ public class Document {
      * @return the number of sentences in the document
      */
     public int numSentences() {
-        // TODO: Implement this method
-        return 0;
-    }
+        int sentenceCount = 0;
+        StringBuilder lowDocText = new StringBuilder();
 
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+        iterator.setText(DocText.toString());
+        int start = iterator.first();
+        for (int end = iterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = iterator.next()) {
+
+            String sentence = DocText.toString().substring(start, end);
+           // System.out.println(sentence);
+            sentenceCount++;
+        }
+        return sentenceCount;
+    }
     /**
      * Obtain a specific sentence from the document.
      * Sentences are numbered starting from 1.
@@ -75,18 +279,67 @@ public class Document {
      * @return the sentence indexed by {@code sentence_number}
      */
     public String getSentence(int sentence_number) {
-        // TODO: Implement this method
-        return null;
+        int sentenceCount = 0;
+        ArrayList<String> lowDocText = new ArrayList<>();
+
+        BreakIterator iterator = BreakIterator.getSentenceInstance(Locale.US);
+        iterator.setText(DocText.toString());
+        int start = iterator.first();
+        for (int end = iterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = iterator.next()) {
+
+            String sentence = DocText.toString().substring(start, end);
+            lowDocText.add(sentence);
+
+        }
+
+        return lowDocText.get(sentence_number-1).trim(); //remove any trailing spaces
     }
 
     public double averageSentenceLength() {
-        // TODO: Implement this method
-        return 0.0;
+        String lowDocText2 = new String(DocText.toString());
+        lowDocText2 = DocText.toString().toLowerCase();
+
+        ArrayList<String> wordArrayList = new ArrayList<String>();
+        for(String word : lowDocText2.split(" ")){
+            if(word.length() != 0){
+                if(word.length() == 1){
+                    if(((word.charAt(0) >= 'a' && word.charAt(0) <= 'z'))){
+                        wordArrayList.add(word);
+                    }
+                }
+                else {
+                    wordArrayList.add(word);
+                }
+            }
+        }
+
+
+        return (double) wordArrayList.size()/numSentences();
     }
 
     public double averageSentenceComplexity() {
-        // TODO: Implement this method
-        return 0.0;
+        String lowDocText2 = new String(DocText.toString());
+        lowDocText2 = DocText.toString().toLowerCase();
+
+        ArrayList<String> phraseArrayList = new ArrayList<String>();
+        for(String phrase : lowDocText2.split("[,;:.!?]")){
+            if(phrase.length() != 0){
+                if(phrase.length() == 1){
+                    if(((phrase.charAt(0) >= 'a' && phrase.charAt(0) <= 'z'))){
+                        phraseArrayList.add(phrase);
+                        System.out.println(phrase);
+                    }
+                }
+                else {
+                    phraseArrayList.add(phrase);
+                    System.out.println(phrase);
+                }
+            }
+        }
+
+        return (double) (phraseArrayList.size()-1)/numSentences();
     }
 
     /* ------- Task 3 ------- */

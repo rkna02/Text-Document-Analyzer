3cd412b2a383d4efc7660672d62b3bed3a115cd5
JerryShao3234 <shaojerry@icloud.com>
2021-10-07 11:31:24 -0700

    Task 4 is done!

---
+++ src/test/java/cpen221/mp1/Task4SmokeTests.java
@@ -0,0 +1,59 @@
+package cpen221.mp1;
+
+import cpen221.mp1.similarity.DocumentSimilarity;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Task4SmokeTests extends DocumentSimilarity {
+
+    private static Document testDocument1;
+    private static Document testDocument2;
+    private static Document testDocument3;
+    private static Document testDocument4;
+    private static Document testDocument5;
+
+    @BeforeAll
+    public static void setupTests() throws MalformedURLException {
+        testDocument1 = new Document("Task four test document one", "resources/task4TestDoc1.txt");
+        testDocument2 = new Document("Task four test document two", "resources/task4TestDoc2.txt");
+        testDocument3 = new Document("Task four test document three", "resources/task4TestDoc3.txt");
+        testDocument4 = new Document("Task four test document four", "resources/task4TestDoc4.txt");
+        testDocument5 = new Document("The Ant and The Cricket", "resources/antcrick.txt");
+
+    }
+
+    @Test
+    public void antcrick() {
+        Assertions.assertEquals(0.0, documentDivergence(testDocument5,testDocument5), 0.005);
+    }
+
+    @Test
+    public void jsDivergenceSameDocument() {
+        Assertions.assertEquals(0.0, documentDivergence(testDocument1,testDocument1), 0.005);
+    }
+
+    @Test
+    public void divergenceSameDocument() {
+        Assertions.assertEquals(0.0, documentDivergence(testDocument1,testDocument1), 0.005);
+    }
+
+    @Test
+    public void DivergenceSmallDifference() {
+        Assertions.assertEquals(1.24345, documentDivergence(testDocument1,testDocument2), 0.005);
+    }
+
+    @Test
+    public void DivergenceBigDifference() {
+        Assertions.assertEquals(7.6266, documentDivergence(testDocument1,testDocument3), 0.005);
+    }
+
+    @Test
+    public void DivergenceNoRepeatedWords() {
+        System.out.println(documentDivergence(testDocument1,testDocument4));
+        Assertions.assertEquals(44.401, documentDivergence(testDocument1,testDocument4), 0.005);
+    }
+}
